cd4287d864a1a91c28c10e64a3382f5be91be4c1
Aravinth <79074310+Aravinth1525@users.noreply.github.com>
2022-09-17 00:34:41 +0530

    Subplot - First Order Derivative

---
+++ DA 2/First_Derivative.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+from skimage import filters
+from skimage.data import camera
+image = camera()
+edge_roberts = filters.roberts(image)
+edge_sobel = filters.sobel(image)
+edge_prewitt=filters.prewitt(image)
+fig, axes = plt.subplots(ncols=4, sharex=True, sharey=True,figsize=(8, 4))
+axes[0].imshow(image, cmap=plt.cm.gray)
+axes[0].set_title('Input Image')
+axes[1].imshow(edge_roberts, cmap=plt.cm.gray)
+axes[1].set_title('Roberts Image')
+axes[2].imshow(edge_prewitt, cmap=plt.cm.gray)
+axes[2].set_title('Prewitt Edge Detection')
+axes[3].imshow(edge_sobel, cmap=plt.cm.gray)
+axes[3].set_title('Sobel Edge Detection')
+for ax in axes:
+    ax.axis('off')
+plt.tight_layout()
+plt.show()
