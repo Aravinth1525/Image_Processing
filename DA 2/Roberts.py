faaa2a77cce8d42f7c4712c054d122058da6902d
Aravinth <79074310+Aravinth1525@users.noreply.github.com>
2022-09-17 22:20:14 +0530

    Delete DA 2 directory

--- DA 2/Roberts.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import cv2
-from skimage import filters
-camera = cv2.imread('Camera.jpeg',2)
-edge_roberts = filters.roberts(camera)
-plt.subplot(121),plt.imshow(camera),plt.title("Input Image")
-plt.subplot(122),plt.imshow(edge_roberts),plt.title("Roberts")
-
--- DA 2/Sobel.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import cv2
-from skimage import filters
-camera = cv2.imread('Camera.jpeg')
-edge_sobel = filters.sobel(camera)
-plt.subplot(121),plt.imshow(camera),plt.title("Input Image")
-plt.subplot(122),plt.imshow(edge_sobel),plt.title("Sobel")
-
